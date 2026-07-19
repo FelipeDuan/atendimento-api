77cc26db86bdddab71ecc308c07f9dcce158f224
Felipe Duan <felipeduan1@gmail.com>
2026-07-19 14:15:50 -0300

    init: estrutura inicial da api

---
+++ src/test/java/com/felipeduan/atendimento/AtendimentoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.felipeduan.atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/felipeduan/atendimento/TestAtendimentoApplication.java
@@ -0,0 +1,11 @@
+package com.felipeduan.atendimento;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAtendimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AtendimentoApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
