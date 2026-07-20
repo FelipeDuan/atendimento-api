e86a614c0e6f5f50246cc1e55b6259f270c48c35
Felipe Duan <felipeduan1@gmail.com>
2026-07-19 21:25:19 -0300

    feat(shared): adicionado TenancyConfig para transações

---
+++ src/main/java/com/felipeduan/atendimento/shared/config/TenancyConfig.java
@@ -0,0 +1,12 @@
+package com.felipeduan.atendimento.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAspectJAutoProxy
+@EnableTransactionManagement(order = 0)
+public class TenancyConfig {
+    
+}
