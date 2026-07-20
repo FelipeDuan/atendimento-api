3736514288f7abb7916ba45c9be459bf10576d39
Felipe Duan <felipeduan1@gmail.com>
2026-07-19 23:02:02 -0300

    test: AbstractIntegrationTest extendido no smoke de contexto

--- src/test/java/com/felipeduan/atendimento/AtendimentoApplicationTests.java
+++ src/test/java/com/felipeduan/atendimento/AtendimentoApplicationTests.java
@@ -1,12 +1,8 @@
 package com.felipeduan.atendimento;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class AtendimentoApplicationTests {
+class AtendimentoApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
