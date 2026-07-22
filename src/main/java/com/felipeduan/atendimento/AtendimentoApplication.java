ccc3f6ce7fae4091d8e4f5bd1ea415f324a0f780
Felipe Duan <felipeduan1@gmail.com>
2026-07-22 12:12:44 -0300

    docs(openapi): estabilizou operationIds e tipou a config da Meta

--- src/main/java/com/felipeduan/atendimento/AtendimentoApplication.java
+++ src/main/java/com/felipeduan/atendimento/AtendimentoApplication.java
@@ -1,11 +1,12 @@
 package com.felipeduan.atendimento;
 
 import com.felipeduan.atendimento.shared.config.JwtProperties;
+import com.felipeduan.atendimento.shared.config.MetaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, MetaProperties.class})
 @SpringBootApplication
 public class AtendimentoApplication {
 
--- src/main/java/com/felipeduan/atendimento/modules/platformadmin/PlatformAdminAuthController.java
+++ src/main/java/com/felipeduan/atendimento/modules/platformadmin/PlatformAdminAuthController.java
@@ -2,9 +2,11 @@ package com.felipeduan.atendimento.modules.platformadmin;
 
 import com.felipeduan.atendimento.modules.platformadmin.dto.LoginPlataformaRequest;
 import com.felipeduan.atendimento.modules.platformadmin.dto.LoginPlataformaResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth/plataforma")
 @Tag(name = "Auth")
+@RequiredArgsConstructor
 public class PlatformAdminAuthController {
 
   private final PlatformAdminAuthService authService;
 
-  public PlatformAdminAuthController(PlatformAdminAuthService authService) {
-    this.authService = authService;
-  }
-
   @PostMapping("/login")
   @SecurityRequirements
+  @Operation(operationId = "loginPlataforma")
   public LoginPlataformaResponse login(@Valid @RequestBody LoginPlataformaRequest request) {
     return authService.autenticar(request);
   }
